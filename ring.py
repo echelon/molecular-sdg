74fbde05d4a06ce1a3490144adc6d0fa9f8af193
Brandon Thomas Suit <brandon.suit@gmail.com>
2011-07-25 12:50:27 -0400

    Moving ring support classes into separate module

---
+++ ring.py
@@ -0,0 +1,249 @@
+"""
+Ring-related data structures.
+"""
+
+from util.enum import enum
+
+RING_TYPES = enum(
+	'CORE', 		# Last remaining ring (tractable case)
+	'TOUGH_CORE',	# Remaining ring(s) (intractable case)
+	'FUSED', 'BRIDGED', 'SPIRO', # Types of peeled rings 
+	'IRREGULAR',	# Irregular ring
+	'NONE'			# No type assigned
+)
+
+class RingSystem(object):
+	"""
+	Contains all of the rings in the molecule.
+	"""
+	pass
+
+
+class RingGroup(object):
+	"""
+	Contains all rings that are connected together.
+	Eg. "PhPh-C-Ph-C-PhPhPh" contains 3 ring groups.
+
+	Useful for saving on computation legwork later: we can restrict
+	searches or traversals to be within the group instead of searching
+	the entire molecule's ring system.
+	"""
+	# TODO: Give iterator interface, or derive from tuple class.
+
+	def __init__(self, rings, rgId = None):
+		"""
+		Input: a list of Ring objects.
+		"""
+		self.rings = rings
+
+		# An arbitrary ID that the grouping algorithm assigns. Should
+		# be unique for every ring group in the molecule, but it is 
+		# not mandatory.
+		self.rgId = rgId
+
+	def getRings(self):
+		"""Return a copy of the ring list."""
+		return self.rings[:]
+
+	def hasRing(self, ring):
+		"""Returns whether the Ring belongs to the group."""
+		return ring in self.rings
+
+	def __repr__(self):
+		"""Debug representation."""
+		if self.rgId == None:
+			return "<RingGroup(%d rings)>" % len(self.rings)
+		return "<RingGroup(#%d, %d rings)>" % (self.rgId, len(self.rings))
+
+	def __str__(self):
+		"""String representation."""
+		ret = "Ring Group"
+		if self.rgId != None:
+			ret += " (#%d)" % self.rgId
+		for ring in self.rings:
+			ret += "\n  * " + str(ring)
+		return ret
+
+class Ring(object):
+	"""
+	Ring Data Structure.
+	Contains the atom cycle in the ring, bonds, etc.
+	Also assists in processing tasks.
+	"""
+	# FIXME: Remove most of the methods. They belong in analysis only!
+
+	def __init__(self, path):
+		"""
+		Ring Constructor
+		Must specify the atom cycle that constitutes the ring.
+		"""
+
+		# The path of atoms in the ring. 
+		# Each atom only occurs once, so start and end are bonded.
+		# (Technically, rings are undirected cycles.)
+		self.ringPath = None
+
+		# Bonds in the ring. 
+		# A list of sets; each set is an edge.
+		self.bonds = None
+
+		# The type of ring (elucidated during ring peeling).
+		self.type = RING_TYPES.NONE
+
+		def build_path(path):
+			"""
+			Duplicate the list, then remove the end atom if it is also
+			the start atom.
+			"""
+			newPath = path[:]
+			if newPath[0] == newPath[-1]:
+				newPath.pop()
+			return newPath
+
+		def build_bonds(path):
+			"""Build a list of sets that represent the bonds."""
+			l = len(path)
+			bonds = []
+			for i in range(l):
+				a = path[i]
+				a2 = path[(i+1)%l]
+				bonds.append(set([a, a2]))
+			return bonds
+
+		# Build.
+		self.ringPath = build_path(path)
+		self.bonds = build_bonds(self.ringPath)
+
+	def isCentralRing(self, ringList):
+		"""
+		Determine ring centrality.
+		If a ring is central, its removal will partition the remining
+		rings. We want to peel the rings on the molecule extremety
+		first. 
+
+		This algorithm is modified from [Helson] in order to compensate
+		for molecules such as PhPh-PhPhPh.
+		"""
+		rings = ringList[:]
+		numRings = len(rings)
+
+		# Remove our ring from the system
+		pos = None
+		for i in range(len(rings)):
+			if rings[i] == self:
+				pos = i 
+				break
+		
+		if pos != None:
+			rings.pop(pos)
+			numRings -= 1
+
+		# Arbitrarily choose starting ring. To compensate for an issue
+		# with Helson's algoritm, we'll choose the first that is spiro
+		# or fused to the current ring. 
+		rootPos = None
+		for i in range(len(rings)):
+			if self.isSpiroTo(rings[i]) or self.isFusedTo(rings[i]):
+				rootPos = i
+				break
+
+		if rootPos == None:
+			rootPos = 0
+
+		# Iteratively collect rings that are fused/spiro.
+		# If the number collected equals the number of rings (not
+		# including this one), then this ring is not "central". 
+		collected = [rootPos]
+		justAdded = [rootPos]
+		while len(justAdded) != 0:
+			remRings = [x for x in range(len(rings)) if x not in collected]
+			added = []
+			for r in justAdded:
+				for s in remRings:
+					if rings[r].isSpiroTo(rings[s]) or \
+							rings[r].isFusedTo(rings[s]):
+						collected.append(s)
+						added.append(s)
+			justAdded = added
+
+		if len(collected) == numRings:
+			return False
+
+		# If not all rings were gathered, perhaps the ring system isn't
+		# "connected". Perform the collection again, this time with 
+		# this ring included. Compare the results. If the number of 
+		# collected rings (minus this one) equals the same number as
+		# before, then this ring had no "connecting" effect and is not
+		# central.
+		rings.append(self)
+
+		collected2 = [rootPos]
+		while len(justAdded) != 0:
+			remRings = [x for x in rings if x not in collected]
+			added = []
+			for r in justAdded:
+				for s in remRings:
+					if r.isSpiroTo(rings[s]) or r.isFusedTo(rings[s]):
+						collected.append(s)
+						added.append(s)
+			justAdded = added
+
+		if len(collected2) - 1 == len(collected):
+			return False
+
+		return True
+
+	def isSpiroTo(self, otherRing):
+		"""
+		Returns True if the ring shares only one atom with the other
+		ring.
+		"""
+		count = 0
+		for a in self.ringPath:
+			if a in otherRing.ringPath:
+				count += 1
+		return count == 1
+
+	def isFusedTo(self, otherRing):
+		"""
+		Returns True if the ring shares one bond (edge) with the other
+		ring.
+		"""
+		count = 0
+		for b in self.bonds:
+			if b in otherRing.bonds:
+				count += 1
+		# TODO: Can they share more than one and be considered fused?
+		return count == 1
+
+	def isBridgedTo(self, otherRing):
+		"""
+		Returns True if the ring bridges the other ring.
+		"""
+		# TODO: Bridged ring support
+		#print "TODO: Bridged ring support"
+		return False
+
+	def __str__(self):
+		"""Returns string representation of the object."""
+		# Type => String map. Don't handle 'none'
+		rtype = ''
+		if self.type == RING_TYPES.CORE:
+			rtype = 'Core'
+		elif self.type == RING_TYPES.TOUGH_CORE:
+			rtype = 'ToughCore'
+		elif self.type == RING_TYPES.FUSED:
+			rtype = 'Fused'
+		elif self.type == RING_TYPES.BRIDGED:
+			rtype = 'Bridged'
+		elif self.type == RING_TYPES.SPIRO:
+			rtype = 'Spiro'
+		elif self.type == RING_TYPES.IRREGULAR:
+			rtype = 'Irregular'
+
+		if rtype:
+			return "<%sRing(len=%d, p=%s)>"  % (rtype, len(self.ringPath),
+					self.ringPath)
+
+		return "<Ring(len=%d, p=%s)>" % (len(self.ringPath), self.ringPath)
+
--- sdg/ring_analysis.py
+++ sdg/ring_analysis.py
@@ -3,189 +3,7 @@ Ring analysis.
 This code is adapted from [Helson].
 """
 
-from util.enum import enum
-
-RING_TYPES = enum(
-	'CORE', 		# Last remaining ring (tractable case)
-	'TOUGH_CORE',	# Remaining ring(s) (intractable case)
-	'FUSED', 'BRIDGED', 'SPIRO', # Types of peeled rings 
-	'IRREGULAR',	# Irregular ring
-	'NONE'			# No type assigned
-)
-
-class Ring(object):
-	"""
-	Represents a ring.
-	"""
-
-	def __init__(self, path):
-		"""Constructor."""
-
-		def build_path(path):
-			"""
-			Duplicate the list, then remove the end atom if it is also
-			the start atom.
-			"""
-			newPath = path[:]
-			if newPath[0] == newPath[-1]:
-				newPath.pop()
-			return newPath
-
-		def build_bonds(path):
-			"""Build a list of sets that represent the bonds."""
-			l = len(path)
-			bonds = []
-			for i in range(l):
-				a = path[i]
-				a2 = path[(i+1)%l]
-				bonds.append(set([a, a2]))
-			return bonds
-
-		# The path of atoms in the ring. 
-		#	* Each atom only occurs once, so start and end are bonded.
-		#	* Technically, rings are undirected cycles.
-		self.ringPath = build_path(path)
-
-		# Bonds in the ring. A list of sets; each set is an edge.
-		self.bonds = build_bonds(self.ringPath)
-
-		# The type of ring (elucidated during ring peeling).
-		self.type = RING_TYPES.NONE
-
-	def isCentralRing(self, ringList):
-		"""
-		Determine ring centrality.
-		If a ring is central, its removal will partition the remining
-		rings. We want to peel the rings on the molecule extremety
-		first. 
-
-		This algorithm is modified from [Helson] in order to compensate
-		for molecules such as PhPh-PhPhPh.
-		"""
-		rings = ringList[:]
-		numRings = len(rings)
-
-		# Remove our ring from the system
-		pos = None
-		for i in range(len(rings)):
-			if rings[i] == self:
-				pos = i 
-				break
-		
-		if pos != None:
-			rings.pop(pos)
-			numRings -= 1
-
-		# Arbitrarily choose starting ring. To compensate for an issue
-		# with Helson's algoritm, we'll choose the first that is spiro
-		# or fused to the current ring. 
-		rootPos = None
-		for i in range(len(rings)):
-			if self.isSpiroTo(rings[i]) or self.isFusedTo(rings[i]):
-				rootPos = i
-				break
-
-		if rootPos == None:
-			rootPos = 0
-
-		# Iteratively collect rings that are fused/spiro.
-		# If the number collected equals the number of rings (not
-		# including this one), then this ring is not "central". 
-		collected = [rootPos]
-		justAdded = [rootPos]
-		while len(justAdded) != 0:
-			remRings = [x for x in range(len(rings)) if x not in collected]
-			added = []
-			for r in justAdded:
-				for s in remRings:
-					if rings[r].isSpiroTo(rings[s]) or \
-							rings[r].isFusedTo(rings[s]):
-						collected.append(s)
-						added.append(s)
-			justAdded = added
-
-		if len(collected) == numRings:
-			return False
-
-		# If not all rings were gathered, perhaps the ring system isn't
-		# "connected". Perform the collection again, this time with 
-		# this ring included. Compare the results. If the number of 
-		# collected rings (minus this one) equals the same number as
-		# before, then this ring had no "connecting" effect and is not
-		# central.
-		rings.append(self)
-
-		collected2 = [rootPos]
-		while len(justAdded) != 0:
-			remRings = [x for x in rings if x not in collected]
-			added = []
-			for r in justAdded:
-				for s in remRings:
-					if r.isSpiroTo(rings[s]) or r.isFusedTo(rings[s]):
-						collected.append(s)
-						added.append(s)
-			justAdded = added
-
-		if len(collected2) - 1 == len(collected):
-			return False
-
-		return True
-
-	def isSpiroTo(self, otherRing):
-		"""
-		Returns True if the ring shares only one atom with the other
-		ring.
-		"""
-		count = 0
-		for a in self.ringPath:
-			if a in otherRing.ringPath:
-				count += 1
-		return count == 1
-
-	def isFusedTo(self, otherRing):
-		"""
-		Returns True if the ring shares one bond (edge) with the other
-		ring.
-		"""
-		count = 0
-		for b in self.bonds:
-			if b in otherRing.bonds:
-				count += 1
-		# TODO: Can they share more than one and be considered fused?
-		return count == 1
-
-	def isBridgedTo(self, otherRing):
-		"""
-		Returns True if the ring bridges the other ring.
-		"""
-		# TODO: Bridged ring support
-		#print "TODO: Bridged ring support"
-		return False
-
-	def __str__(self):
-		"""Returns string representation of the object."""
-		# Type => String map. Don't handle 'none'
-		rtype = ''
-		if self.type == RING_TYPES.CORE:
-			rtype = 'core'
-		elif self.type == RING_TYPES.TOUGH_CORE:
-			rtype = 'tough_core'
-		elif self.type == RING_TYPES.FUSED:
-			rtype = 'fused'
-		elif self.type == RING_TYPES.BRIDGED:
-			rtype = 'bridged'
-		elif self.type == RING_TYPES.SPIRO:
-			rtype = 'spiro'
-		elif self.type == RING_TYPES.IRREGULAR:
-			rtype = 'irregular'
-
-		if rtype:
-			return "<Ring/%s(%d): %s>"  % (rtype, len(self.ringPath), 
-					self.ringPath)
-
-		return "<Ring(%d): %s>" % (len(self.ringPath), self.ringPath)
-
-# ===========================
+from ring import *
 
 def ring_analysis(rings):
 	"""
@@ -196,13 +14,93 @@ def ring_analysis(rings):
 	for r in rings:
 		remainingRings.append(Ring(r))
 
-	peelOrder = assign_ring_types(remainingRings)
+	
+	groups = _segment_into_ring_groups(remainingRings)
+	print groups
+
+	for g in groups:
+		print g
+
+	peelOrder = _assign_ring_types(remainingRings)
 
 	# TODO: Repeat again with license for bridged systems. 
 
 	return peelOrder
 
-def assign_ring_types(remainingRings):
+def _segment_into_ring_groups(ringList):
+	"""
+	Segment the list of rings from the perception stage into ring 
+	groups based on the connectivity.
+
+	Input: A list of Ring objects.
+	Output: A list of RingGroup objects.
+
+	This is not based on literature, but aids in analysis and 
+	construction.
+	"""
+
+	def in_same_group(ring1, ring2, ringList):
+		"""
+		Determine if ring1 and ring2 are in the same group by
+		attempting to traverse from one to the other. This is
+		just BFS without the queue.
+		"""
+		# Easy case -- see if the rings are directly connected.
+		if ring1.isSpiroTo(ring2) or ring1.isFusedTo(ring2) or \
+				ring1.isBridgedTo(ring2):
+					return True
+
+		# Iteratively collect rings that are fused/spiro/bridged from
+		# ring1. If we manage to collect ring2, then the two are in the
+		# same connectivity group. This is similar to [Helson]'s 
+		# "central" test algorithm (Procedure D of Ring Analysis, 
+		# p. 334)
+		rings = ringList[:]
+		rings.pop(rings.index(ring1))
+
+		collected = [ring1]
+		justAdded = [ring1]
+		while len(justAdded) != 0:
+			remRings = [x for x in rings if x not in collected]
+			added = []
+			for r in justAdded:
+				for s in remRings:
+					if r.isSpiroTo(s) or r.isFusedTo(s) or r.isBridgedTo(s):
+						collected.append(s)
+						added.append(s)
+			justAdded = added
+
+		# Result.
+		return ring2 in collected
+
+	# Simple case -- only one ring in the entire molecule.
+	if len(ringList) == 1:
+		return [RingGroup(ringList)]
+
+	# More than one ring in molecule -- must group memberships.
+	groups = []
+	ungrouped = ringList[:]
+	justGrouped = []
+	rId = 0
+
+	while len(ungrouped) > 0:
+		groupRoot = ungrouped.pop(0)
+		justGrouped = []
+		for ring in ungrouped:
+			if in_same_group(groupRoot, ring, ringList):
+				justGrouped.append(ring)
+
+		# This is the next ring group.
+		group = [groupRoot]
+		group.extend(justGrouped)
+		groups.append(RingGroup(group, rId))
+		rId += 1
+
+		ungrouped = [x for x in ungrouped if x not in justGrouped]
+
+	return groups
+
+def _assign_ring_types(remainingRings):
 	"""
 	TODO: DOC
 	"""
